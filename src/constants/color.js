e602fc395a78586a2944a5c17af5518284e047af
Vladimir Drannikov <krowen@inbox.ru>
2021-10-27 15:58:57 +0600

    Moved colors into constants

---
+++ src/constants/color.js
@@ -0,0 +1,11 @@
+const colors = {
+  basicColor: '#FFFFFF',
+  accentColor: '#01A7FD',
+  accentColorHover: '#008dd3',
+  accentColorActive: '#00699e',
+  textColor: '#111111',
+  labelColor: 'rgba(17, 17, 17, 0.48)',
+  borderColor: '#F1F1F1',
+}
+
+export default colors;
